28c183e133b0e30ea7d5b8d28fb7c073149fb575
Mario Kukucov <sleepychild@abv.bg>
2020-10-09 06:47:34 +0300

    Save game preview

--- scripts/base/game/game.gd
+++ scripts/base/game/game.gd
@@ -22,28 +22,23 @@ func _on_save_game():
 		save_dir.make_dir_recursive(save_dir_path)
 	var save_file = File.new()
 	save_file.open(save_path ,File.WRITE)
-	
-	# Thumbnail
+	# Thumbnail and Data
 	var img = get_viewport().get_texture().get_data()
 	img.flip_y()
 	img.resize(320, 180)
-	save_file.store_line(to_json({ "thumbnail": Marshalls.raw_to_base64(img.save_png_to_buffer()) }))
-	# Game Data
 	save_file.store_line(to_json({
 			"game_time": game_time,
 			"game_difficulty": game_difficulty,
 			"game_level_loaded": game_level_loaded,
+			"thumbnail": Marshalls.raw_to_base64(img.save_png_to_buffer()),
 			}))
-	
 	for gs_obj in get_tree().get_nodes_in_group("game_save_objects"):
 		save_file.store_line(to_json({
 				"Name" : gs_obj.name,
 				"FileName" : gs_obj.get_filename(),
 				"Path" : gs_obj.get_parent().get_path()
 				}))
-	
 	save_file.close()
-	
 	# Set last saved game
 	ProfileManager.game_data_path = save_path
 	ProfileManager.save_profile()
--- scripts/base/title/continue.gd
+++ scripts/base/title/continue.gd
@@ -5,8 +5,15 @@ onready var close_button = get_close_button()
 onready var games_list = $VBoxContainer/games/GAMES
 onready var saves_list = $VBoxContainer/games/SAVES
 
-onready var game_load_button = $VBoxContainer/BUTTONS/LOAD
-onready var game_cancel_button = $VBoxContainer/BUTTONS/CANCEL
+onready var game_thumbnail = $VBoxContainer/games/GAME_DATA/thumbnail
+onready var game_difficulty = $VBoxContainer/games/GAME_DATA/difficulty
+onready var game_level = $VBoxContainer/games/GAME_DATA/level
+onready var game_time = $VBoxContainer/games/GAME_DATA/time
+
+onready var game_load_button = $VBoxContainer/games/GAME_DATA/LOAD
+onready var game_delete_button = $VBoxContainer/games/GAME_DATA/DELETE
+
+onready var game_cancel_button = $VBoxContainer/CANCEL
 
 func _ready():
 	connect("about_to_show", self, "list_games")
@@ -20,6 +27,8 @@ func _ready():
 	saves_list.connect("item_activated", self, "_on_save_activated")
 
 	game_load_button.connect("pressed", self, "_on_game_load_button_pressed")
+	game_delete_button.connect("pressed", self, "_on_game_delete_button_pressed")
+	
 	game_cancel_button.connect("pressed", self, "_on_game_cancel_button_pressed")
 
 	game_load_button.grab_focus()
@@ -27,8 +36,11 @@ func _ready():
 func _on_game_load_button_pressed():
 	pass
 
-func _on_game_cancel_button_pressed():
+func _on_game_delete_button_pressed():
 	pass
+
+func _on_game_cancel_button_pressed():
+	hide()
 
 func list_games():
 	games_list.clear()
@@ -56,7 +68,23 @@ func _on_game_activated_selected(item_index):
 func _on_save_selected(item_index):
 	var game_index = int( games_list.get_item_text( games_list.get_selected_items()[0] ) )
 	ProfileManager.get_game_save_list( game_index )
-	print( ProfileManager.game_save_list[item_index] )
+	var save_path = ProfileManager.get_game_save_path(game_index) + ProfileManager.game_save_list[item_index]
+	var save_file = File.new()
+	save_file.open(save_path ,File.READ)
+	var game_data = parse_json( save_file.get_line() )
+	#game_thumbnail
+	var thumb_image = Image.new()
+	var thumb_image_texture = ImageTexture.new()
+	if game_data.has("thumbnail"):
+		thumb_image.load_png_from_buffer( Marshalls.base64_to_raw( game_data.thumbnail ) )
+		thumb_image_texture.create_from_image( thumb_image )
+		game_thumbnail.set_texture( thumb_image_texture )
+	else:
+		thumb_image_texture.create_from_image( thumb_image )
+		game_thumbnail.set_texture( thumb_image_texture )
+	game_difficulty.set_text( game_data.game_difficulty )
+	game_level.set_text( game_data.game_level if game_data.has("game_level") else "" )
+	game_time.set_text( String( game_data.game_time ) if game_data.has("game_time") else "" )
 
 func _on_save_activated(item_index):
 	print("save activated ", item_index)
