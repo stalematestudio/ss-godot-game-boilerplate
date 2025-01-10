2a7370f089dd4b16d5dca431fd21e78db7a8ace2
Mario Kukutsov <kukucov@gmail.com>
2025-01-10 17:38:47 +0200

    Camera zoom keys can be held.

--- player/scripts/player_camera.gd
+++ player/scripts/player_camera.gd
@@ -2,7 +2,7 @@ class_name PlayerCamera extends Camera3D
 
 # Camera positions
 @export var camera_position: Array[Vector3] = [
-	Vector3(),Vector3(0,0,-1),Vector3(0,.5,-2)
+	Vector3(),Vector3(0,0,-1),Vector3(0,0.5,-2)
 ]
 @onready var camera_position_selected = 0
 
@@ -13,13 +13,12 @@ func _ready():
 func _on_camera_config_changed():
 	set_fov(ConfigManager.config_data.video.fov)
 
+func _process(_delta: float) -> void:
+	set_fov(fov + Input.get_axis("zoom_in", "zoom_out"))
+
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("util_camera_switch"):
 		camera_position_selected = camera_position_selected + 1 if camera_position_selected < camera_position.size() - 1 else 0
 		position = camera_position[camera_position_selected]
-	if event.is_action_pressed("zoom_in"):
-		set_fov(fov-1)
-	if event.is_action_pressed("zoom_out"):
-		set_fov(fov+1)
 	if event.is_action_pressed("zoom_reset"):
 		set_fov(ConfigManager.config_data.video.fov)
